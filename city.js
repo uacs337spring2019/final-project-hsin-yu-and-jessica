38d9b66eb7ad93c19b82da114d4694a3f806c27d
hchen2017 <hchen2@email.arizona.edu>
2019-04-11 16:30:35 -0700

    Updated comments

--- city.js
+++ city.js
@@ -17,64 +17,17 @@ app.use(function(req, res, next) {
 
 app.use(express.static('public'));
 
-/*
-
-function getComponents(rawData) {
-  let object = {};
-
-  let components = rawData.split(":");
-  if (components != '') {
-    object["type"] = components[0];
-    object["position"] = components[1];
-    object["color"] = components[2];
-    object["id"] = components[3];
-  }
-
-  return object;
-}
-
-function getObjects(objectsFromData) {
-  let arrOfObjects = [];
-
-  for (let o = 0; o < objectsFromData.length; o++) {
-    let object = getComponents(objectsFromData[o]);
-    arrOfObjects.push(object);
-  }
-
-  return allObjects;
-}
-
-*/
-
 app.get('/', function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
 
   res.header("Content-Type", "application/json");
 
   res.send(fs.readFileSync("scene.json", 'utf8'));
-
-  /*
-
-  let allObjects = {};
-
-  // if getting from a file...
-  let file = "objects.txt";
-  let data = fs.readFileSync(file, 'utf8');
-
-  let objectsFromData = data.split("\n");
-
-  let arrOfObjects = getObjects(objectsFromData.slice(1, objectsFromData.length));
-  allObjects["objects"] = arrOfObjects;
-
-  allObjects["viewPosition"] = objectsFromData[0];
-  res.send(JSON.stringify(allObjects));
-
-  */
 })
 
 /*
 This receives JSON objects to parse and check to make sure JSON has all of the correct
-information necessary to store into scene.json file
+information necessary to store into scene.json file.
 */
 app.post('/object', jsonParser, function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -85,7 +38,7 @@ app.post('/object', jsonParser, function(req, res) {
   let id = req.body.id;
 
   if (type !== "" && position !== "" && color !== "" && id !== "") {
-    // get "objects" (which is an array of objects) to append to that array of objects
+    // get "objects" (which is an array of objects) to append json to that array of objects
     let json = fs.readFileSync("scene.json", 'utf8');
     let scene = JSON.parse(json);
     let objects = scene["objects"];
@@ -93,22 +46,13 @@ app.post('/object', jsonParser, function(req, res) {
 
     console.log("Object saved!");
     res.send("Object saved!");
-
-    /*
-
-    fs.appendFile("scene.json", req, function(error) {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      console.log("Object saved successfully!");
-      res.send("Object saved!");
-    });
-
-    */
   }
 })
 
+/*
+This receives a JSON object containing the viewPosition. The JSON object received is checked
+to make sure it is not emppty and then appended to scene.json under viewPosition.
+*/
 app.post('/view', jsonParser, function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
 
