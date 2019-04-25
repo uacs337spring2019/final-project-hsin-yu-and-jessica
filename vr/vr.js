55d2f8980184128f664ef5aa4a5e015b6dff2d4a
Jessica Coan <jcoan188@gmail.com>
2019-04-24 17:02:25 -0700

    Buildings complete, updating implemented

--- vr/vr.js
+++ vr/vr.js
@@ -3,53 +3,46 @@
   var AFRAME = require('aframe');
   var socket = require('socket.io-client')("http://localhost:3000");
 
-  require('aframe-animation-timeline-component');
-  require('aframe-billboard-component');
-  require('aframe-event-set-component');
-  require('aframe-fps-counter-component');
-  require('aframe-look-at-component');
-  require('aframe-particle-system-component');
-  require('aframe-physics-system');
-  require('aframe-physics-extras');
-  require('aframe-randomizer-components');
-  require('aframe-text-geometry-component');
-
   socket.on('update', function () {
-    getWorld();
+    getScene();
   });
 
-  function start() {
-    getWorld();
-  }
+  window.addEventListener('load', getScene);
 
-  function getWorld() {
+  /**
+   * Initiates the GET request to get the scene
+   */
+  function getScene() {
     console.log("getting world");
     fetch("http://localhost:3000/scene")
       .then(checkStatus)
-      .then(updateWorld);
+      .then(updateScene);
   }
 
-  function updateWorld(data) {
-     let json = JSON.parse(data);
+  /**
+   * Updates the scene based on new data. Inserts new objects in if needed, otherwise updates current objects.
+   * @param {String} data Data recieved from the server representing the scene
+   */
+  function updateScene(data) {
+    let json = JSON.parse(data);
+    document.getElementById("viewer").object3D.position.set(json.viewPosition[0], 2, json.viewPosition[1]);
 
-     for (let i = 0; i < json.objects.length; i++) {
-       if (document.getElementById(json.objects[i].id) !== null) {
-         continue;
-       }
+    if (json.objects.length == 0) {
+      document.getElementById("level-buildings").innerHTML = "";
+    }
 
-       let newBuilding = document.createElement("a-entity");
-       newBuilding.id = json.objects[i].id;
-       newBuilding.setAttribute('mixin', json.objects[i].type);
-       newBuilding.object3D.position.set(json.objects[i].position[0], 0, json.objects[i].position[1]);
-       newBuilding.setAttribute('material', 'color', json.objects[i].color);
-       document.getElementById("level").appendChild(newBuilding);
-     }
+    for (let i = 0; i < json.objects.length; i++) {
+      let item = document.getElementById(json.objects[i].id)
+      if (item === null) {
+        item = newBuilding = document.createElement("a-entity");
+        item.id = json.objects[i].id;
+        document.getElementById("level-buildings").appendChild(item);
+      }
 
-     document.getElementById("viewer").object3D.position = new THREE.Vector3(json.viewPosition[0], 2, json.viewPosition[1]);
-  }
-
-  function getRandomInt(min, max) {
-    return Math.floor((Math.random() * max) + min);
+      item.setAttribute('mixin', json.objects[i].type);
+      item.object3D.position.set(json.objects[i].position[0], 0, json.objects[i].position[1]);
+      item.setAttribute('material', 'color', json.objects[i].color);
+    }
   }
 
   /**
@@ -63,6 +56,4 @@
       return Promise.reject(new Error(response.status + ": " + response.statusText));
     }
   }
-
-  window.addEventListener('load', start);
 })();
